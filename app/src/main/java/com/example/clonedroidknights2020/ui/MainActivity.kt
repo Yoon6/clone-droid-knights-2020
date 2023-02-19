83761e7379e3c1a817e1f30ccaa5206f5c3bce16
yoon6 <yoonda5898@gmail.com>
2023-02-19 23:16:12 +0900

    set bottom nav

--- app/src/main/java/com/example/clonedroidknights2020/ui/MainActivity.kt
+++ app/src/main/java/com/example/clonedroidknights2020/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.clonedroidknights2020
+package com.example.clonedroidknights2020.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clonedroidknights2020.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
